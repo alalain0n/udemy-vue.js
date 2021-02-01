18f97c486da000cf2c1c1373bf16336ef7d341bc
alalain0n <alalain0n@yahoo.co.jp>
2021-02-01 22:14:39 +0900

    first commit

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        firstName: "Jack",
+        lastName: "Donald",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+div {
+  border: 1px solid blue;
+}
+
+h1 {
+  color: red;
+}
+</style>
