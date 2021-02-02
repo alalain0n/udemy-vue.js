4182c30d366c7f2d95335cb2faaa5c7035df39c4
alalain0n <alalain0n@yahoo.co.jp>
2021-02-03 02:01:46 +0900

    210202

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,33 @@
       <label for="maxNumber">最大人数</label>
       <input id="maxNumber" type="number" v-model.number="eventData.title">
       <p>{{eventData.title}}</p>
+      <label for="host">主催者</label>
+      <input id="host" type="text" v-model.trim="eventData.host">
+      <p>{{eventData.host}}</p>
+      <label for="detail">イベントの内容</label>
+      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <p>{{eventData.detail}}</p>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{eventData.isPrivate}}</p>
+      <p>参加条件</p>
+      <input type="checkbox" id="10" value="10代" v-model="eventData.target">
+      <label for="10">10代</label>
+      <input type="checkbox" id="20" value="20代" v-model="eventData.target">
+      <label for="20">20代</label>
+      <input type="checkbox" id="30" value="30代" v-model="eventData.target">
+      <label for="20">30代</label>
+      <p>{{eventData.target}}</p>
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.price">
+      <label for="paid">有料</label>
+      <p>開催場所</p>
+      <select v-model="eventData.location">
+        <option v-for="location in locations" :key="location">{{location}}</option>
+      </select>
+      <p>{{eventData.location}}</p>
     </div>
   </div>
 </template>
@@ -28,9 +55,15 @@ export default {
     return {
       number: 14,
       currentComponent: "Home",
+      locations: ["東京","大阪","名古屋"],
       eventData: {
-        title: "タイトル",
+        title: "",
         maxNumber: 0,
+        host: "",
+        isPrivate: false,
+        target: [],
+        price: "無料",
+        location: "東京",
       },
     };
   },
