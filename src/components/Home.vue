9e4f1e37b1acc2ecb21adb1e0739384f9a99e02c
kazuki aratani <alalain0n@gmail.com>
2021-02-05 21:40:18 +0900

    210205off

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,37 @@
 <template>
-  <p>Home</p>
+  <div>
+    <p v-border:solid.round.shadow="{width: '5px', color: 'brown'}">Home</p>
+    <h2>{{title | lowerCase | upperCase}}</h2>
+    <p>{{subTitle | lowerCase}}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tmpData: "hello",
+      title: "welcome to Tokyo",
+      subTitle: "Tokyo is a great city"
+    };
+  },
+  filters: {
+    lowerCase(value) {
+      return value.toLowerCase();
+    }
+  },
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+      }
+    }
+  }
+}
+</script>
